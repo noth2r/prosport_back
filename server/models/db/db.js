14388f36647a17dc9e77d4b6db637f602ca06d7c
xw_ed <shadowsonics@mail.ru>
2022-02-15 18:09:53 +0000

    Fix

--- server/models/db/db.js
+++ server/models/db/db.js
@@ -1,69 +1,53 @@
-"use strict"
+"use strict";
 
-const mysql = require('mysql2')
+const mysql = require("mysql2");
 
 class db {
-    constructor() {
-        this.dbConfig = {
-            host: process.env.MYSQL_HOST,
-            database: process.env.MYSQL_DB,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
-        }
-    }
+  constructor() {
+    this.dbConfig = {
+      host: process.env.MYSQL_HOST,
+      database: process.env.MYSQL_DB,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+    };
+  }
 
-    get tables() {
-        return {
-            products: {
-                name: "`products`",
-                keys: "(`product_category`, `product_subcategory`, `purchases`, `title`, `description`, `units`, `price`, `quantity`, `amount`, `img_path`)"
-            },
-            tokens: {
-                name: "`tokens`",
-                keys: "(`user`, `refresh_token`)"
-            },
-            admins: {
-                name: "`managers`",
-                keys: "(`email`, `password`, `is_activated`, `activation_link`)"
-            },
-            orders: {
-                name: "`orders`",
-                keys: "(`product_id`, `mail`, `phone`, `address`, `comment`, `amount`)"
-            },
-        }
-    }
+  get tables() {
+    return {
+      products: {
+        name: "`products`",
+        keys: "(`product_category`, `product_subcategory`, `purchases`, `title`, `description`, `units`, `price`, `quantity`, `amount`, `img_path`)",
+      },
+      tokens: {
+        name: "`tokens`",
+        keys: "(`user`, `refresh_token`)",
+      },
+      admins: {
+        name: "`managers`",
+        keys: "(`email`, `password`, `is_activated`, `activation_link`)",
+      },
+      orders: {
+        name: "`orders`",
+        keys: "(`product_id`, `mail`, `phone`, `address`, `comment`, `amount`)",
+      },
+    };
+  }
 
-    async query(query) {
-        // Подключение
-        const connection = mysql.createConnection(this.dbConfig)
+  query(query) {
+    let response = null
+    let connection = mysql.createConnection(this.dbConfig)
 
-        // Ответ базы данных в виде промиса
-        const res = connection.promise().query(query)
-            .then(([rows, fields], err) => {
-                // Если есть ошибка вывести ее
-                if(err) {
-                    throw new Error(err)
-                }
-
-                // Иначе вернуть положительный ответ
-                // с полученными данными
-                return rows
-            })
-            .catch(rej => {
-                throw new Error(rej)
-            })
-
-        // Завершить подключение
-        connection.end((err) => {
-            // В случае ошибки вывести ее
-            if(err) {
-                throw new Error(err)
-            }
+    connection
+        .promise()
+        .query(query)
+        .then(([ rows ]) => response = rows)
+        .then(connection.end())
+        .catch(error => {
+            throw new Error(error)
         })
 
-        // Вернуть ответ сервера
-        return res
-    }
+    return response;
+  }
 }
 
-module.exports = new db
+module.exports = new db();
